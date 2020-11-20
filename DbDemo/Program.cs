c414bcf741fcd9a1fc451fe652476e548dc725c4
Karolis Uosis <kuosis@alna.lt>
2020-11-20 10:16:18 +0200

    Empty project

---
+++ DbDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
